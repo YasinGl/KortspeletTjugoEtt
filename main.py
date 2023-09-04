8a809698ad8a2dfe3e88d74f280a9620a733c6ac
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-04 09:00:59 +0200

    Test

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+def rakna_poang(hand):
+    poang = sum(hand)
+    if 11 in hand and poang > 21:
+        hand.remove(11)
+        hand.append(1)
+    return  poang
+def räkna_poang(hand):
+    poang = sum(hand)
+    if 11 in hand and poang > 21:
+        hand.remove(11)
+        hand.append(1)
+    return poang
+
