59e8856e415e518ce8ae66c664c3aa06b0782c22
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-06 09:29:35 +0200

    Uppdatering

--- main.py
+++ main.py
@@ -65,6 +65,6 @@ def tjugoett():
             print("Dealern vinner. Du kanske borde prova ett annat spel?")
     else:
         print("Vänligen svara JA/NEJ!?")
-        visa_hand()  # Om spelaren inte svarar "ja" eller "nej", starta om spelet.
+        tjugoett()  #  Om spelaren inte svarar "ja" eller "nej", starta om spelet.
 
 tjugoett()  #Kör spelet när programmet körs.
